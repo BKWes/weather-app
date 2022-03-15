249e8994bbdd4f701edd186edd6a80b8e110af22
The Creator <barnesweston@gmail.com>
2022-03-15 16:53:25 -0500

    add api call

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=cc2a72f395e0816b5bafe5bd96ddb5fd';
+
+fetch(apiUrl)
+.then(function(response) {
+    console.log(response);
+    return response.json();
+})
+.then(function(data) {
+    console.log(data);
+})
