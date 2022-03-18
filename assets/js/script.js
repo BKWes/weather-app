6ff285e411c11bb8d3931b3fc8eeca26c014364c
The Creator <barnesweston@gmail.com>
2022-03-18 11:11:56 -0500

    add city weather function to call API and console log weather information for searched city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,37 @@
-<<<<<<< HEAD
 // declare variables
+const appId = 'cc2a72f395e0816b5bafe5bd96ddb5fd'
 var citySearchEl = document.querySelector('#city');
 var weatherEl = document.querySelector('#weather-container');
 var forecastEl = document.querySelector('#five-day-forecast');
 var submitBtn = document.querySelector('button');
-
+var cityName = null
+var city = [];
 
 var formEventHandler = function(event) {
     event.preventDefault(); // prevent browser from refreshing on submit
     let city = citySearchEl.value.trim();
-    console.log(city);
+    if (city) {
+        nameSearch();
+    }
+};
+function nameSearch() {
+    cityName = citySearchEl.value.toLowerCase().replace(/\s/g, '+');
+    cityWeather(cityName);
+}
+
+var cityWeather = function(cityName) {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&units=imperial&appid=' + appId;
+    fetch(apiUrl)
+    .then(function(response) {
+        console.log(response);
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data, cityName);
+    })
 };
 
 submitBtn.addEventListener("click", formEventHandler);
 
-var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=cc2a72f395e0816b5bafe5bd96ddb5fd';
-=======
-var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={optional}&appid=cc2a72f395e0816b5bafe5bd96ddb5fd';
->>>>>>> 485042cc4df88efaf695b8440a4830e857d8f440
 
-fetch(apiUrl)
-.then(function(response) {
-    console.log(response);
-    return response.json();
-})
-.then(function(data) {
-    console.log(data);
-})
+
