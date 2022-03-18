2c61c76e3203737e1977f8173f9b0b3bcbfb4ca5
The Creator <barnesweston@gmail.com>
2022-03-18 10:46:11 -0500

    add form event handler and event listener; currently console logs the city entered into the search input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+// declare variables
+var citySearchEl = document.querySelector('#city');
+var weatherEl = document.querySelector('#weather-container');
+var forecastEl = document.querySelector('#five-day-forecast');
+var submitBtn = document.querySelector('button');
+
+
+var formEventHandler = function(event) {
+    event.preventDefault(); // prevent browser from refreshing on submit
+    let city = citySearchEl.value.trim();
+    console.log(city);
+};
+
+submitBtn.addEventListener("click", formEventHandler);
+
 var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid=cc2a72f395e0816b5bafe5bd96ddb5fd';
 
 fetch(apiUrl)
