7c29d4efc2d81a325c843c046731f503b5b917ee
The Creator <barnesweston@gmail.com>
2022-03-21 19:26:21 -0500

    add html elements for current weather box and get info from fetchAPI in script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,43 +5,42 @@ var currentCityNameEl = document.querySelector('#current-city');
 var weatherEl = document.querySelector('#weather-container');
 var forecastEl = document.querySelector('#five-day-forecast');
 var submitBtn = document.querySelector('button');
-var cityName = null
+var fiveDay = document.querySelector('#five-day');
+var iconEl = document.querySelector('#icon');
+var cityName = null;
 var city = null;
-var date = moment().format("MMM Do YY");
+var date = moment().format("LL");
 
-var formEventHandler = function(event) {
+function formEventHandler(event) {
     event.preventDefault(); // prevent browser from refreshing on submit
     let city = citySearchEl.value.trim();
     if (city) {
         nameSearch();
     }
 };
+
 function nameSearch() {
     cityName = citySearchEl.value.toLowerCase().replace(/\s/g, '+');
-    cityWeather(cityName);
-}
-
-
-var cityWeather = function(cityName) {
-    fetchApi();
-    currentCityNameEl.textContent = citySearchEl.value;
-
-    
-
+    fetchApi(cityName);
 };
 
 submitBtn.addEventListener("click", formEventHandler);
 
 
-function fetchApi() {
+function fetchApi(cityName) {
     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&units=imperial&appid=' + appId;
     fetch(apiUrl)
     .then(function(response) {
-        console.log(response);
         return response.json();
     })
     .then(function(data) {
         console.log(data);
+        currentCityNameEl.textContent = data.name + ' ' + date;
+        iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+
         return data;
+        
     })
+    
+
 };
