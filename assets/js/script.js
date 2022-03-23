03fe4716b7612350479ff2d55457700c0308c95c
Weston <barnesweston@gmail.com>
2022-03-22 20:15:38 -0500

    add second fetch for five day forecast; successfully console.log data from second fetch to create information for daily cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,14 @@
 const appId = 'cc2a72f395e0816b5bafe5bd96ddb5fd'
 var citySearchEl = document.querySelector('#city');
 var currentCityNameEl = document.querySelector('#currentCity');
-var forecastEl = document.querySelector('#five-day-forecast');
 var submitBtn = document.querySelector('button');
 var fiveDay = document.querySelector('#five-day');
 let currentWeatherEl = document.querySelector('#todayBody');
+let dayOneEl = document.querySelector('#day1');
+let dayTwoEl = document.querySelector('#day2');
+let dayThreeEl = document.querySelector('#day3');
+let dayFourEl = document.querySelector('#day4');
+let dayFiveEl = document.querySelector('#day5');
 let iconEl = null;
 var cityName = null;
 var city = null;
@@ -28,13 +32,14 @@ submitBtn.addEventListener("click", formEventHandler);
 
 
 function fetchApi(cityName) {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&units=imperial&appid=' + appId;
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q='+cityName+'&units=imperial&exclude=daily&appid=' + appId;
     fetch(apiUrl)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
         console.log(data);
+        currentCityNameEl.textContent = '';
         // change current city name
         currentCityNameEl.textContent = data.name + ' ' + date;
         // create icon and append to html
@@ -56,8 +61,21 @@ function fetchApi(cityName) {
         // current humidity
         var currentHumid = document.createElement('p');
         currentHumid.setAttribute("class", "text-light bg-dark");
-        currentHumid.textContent = "Wind: "+data.main.humidity;
+        currentHumid.textContent = "Humidity: "+data.main.humidity;
         currentWeatherEl.appendChild(currentHumid);
+        // get coordinates for forecast
+        let latCoord = data.coord.lat;
+        let lonCoord = data.coord.lon;
+        let forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+latCoord+"&lon="+lonCoord+"&units=imperial&exclude=minutely,hourly,alerts&appid="+appId;
+        fetch(forecastUrl)
+            .then(function(responseTwo) {
+                return responseTwo.json()
+            })
+            .then(function(dataTwo) {
+                console.log(dataTwo);
+
+            })
+        // append forecast info to day#El vars
 
         console.log(data);
 
