dad682953acf9c3688ff8e0ad0ef98e184a19dfd
Weston <barnesweston@gmail.com>
2022-03-22 19:50:48 -0500

    create current weather elements and information

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,8 @@ var currentCityNameEl = document.querySelector('#currentCity');
 var forecastEl = document.querySelector('#five-day-forecast');
 var submitBtn = document.querySelector('button');
 var fiveDay = document.querySelector('#five-day');
-var iconEl = document.querySelector('#icon');
+let currentWeatherEl = document.querySelector('#todayBody');
+let iconEl = null;
 var cityName = null;
 var city = null;
 var date = moment().format("LL");
@@ -34,12 +35,34 @@ function fetchApi(cityName) {
     })
     .then(function(data) {
         console.log(data);
+        // change current city name
         currentCityNameEl.textContent = data.name + ' ' + date;
+        // create icon and append to html
+        iconEl = document.createElement('img');
+        iconEl.setAttribute("class", "card-header");
         iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+        currentCityNameEl.append(iconEl);
+        // create todays weather - append to currentWeatherEl
+        // current temp
+        var currentTemp = document.createElement('p');
+        currentTemp.setAttribute("class", "text-light bg-dark");
+        currentTemp.textContent = "Temp: "+data.main.temp;
+        currentWeatherEl.appendChild(currentTemp);
+        // current wind
+        var currentWind = document.createElement('p');
+        currentWind.setAttribute("class", "text-light bg-dark");
+        currentWind.textContent = "Wind: "+data.wind.speed;
+        currentWeatherEl.appendChild(currentWind);
+        // current humidity
+        var currentHumid = document.createElement('p');
+        currentHumid.setAttribute("class", "text-light bg-dark");
+        currentHumid.textContent = "Wind: "+data.main.humidity;
+        currentWeatherEl.appendChild(currentHumid);
 
-        return data;
+        console.log(data);
+
         
     })
     
 
-};
+}; 
